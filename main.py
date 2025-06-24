6340a6cae181c7da029c982c934684e51a37c87f
jessykiruba29 <jessykiruba2006@gmail.com>
2025-06-24 19:55:53 +0530

    backend done

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from pydantic import BaseModel
 import logging
+import joblib
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import nltk
+
+
+nltk.download("stopwords")
+model=joblib.load("logistic_model.pkl")
+vectorizer=joblib.load("tfidf_vectorizer.pkl")
 
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+port_stem=PorterStemmer()
+stop_words=set(stopwords.words('english'))
+def stemming(content):
+    content=re.sub('[^a-zA-Z]', ' ', content)
+    content=content.lower()
+    words=content.split()
+    stemmed=[port_stem.stem(word) for word in words if word not in stop_words]
+    return ' '.join(stemmed)
 
 class News(BaseModel):
     news:str
@@ -14,5 +33,12 @@ class News(BaseModel):
 @app.post("/news")
 async def news_detect(data:News):
     logger.info(f"Received news from user: {data.news}")
-   
+
+    processed=stemming(data.news)
+    vectorized=vectorizer.transform([processed])
+    prediction=model.predict(vectorized)
+
+    result="REAL" if prediction[0]==0 else "FAKE"
+    return {"prediction":result}
+    
 
--- train_model.py
+++ train_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -8,6 +9,12 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer 
 import nltk
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+import logging
+
+
 
 nltk.download('stopwords')
 
@@ -39,26 +46,16 @@ X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,rand
 model=LogisticRegression()
 model.fit(X_train,Y_train)
 
-# model evaluation (training)
-X_train_pred=model.predict(X_train)
-training_accuracy=accuracy_score(X_train_pred,Y_train)
-print(training_accuracy)
+joblib.dump(model, "logistic_model.pkl")
+joblib.dump(vectorizer, "tfidf_vectorizer.pkl")
 
-# model evaluation (testing)
-X_test_pred=model.predict(X_test)
-testing_accuracy=accuracy_score(X_test_pred,Y_test)
-print(testing_accuracy)
+print("✅ Model and vectorizer saved.")
 
-#make a predictor system
-input_data="pope passed away"
-input_data=stemming(input_data)
-input_vector=vectorizer.transform([input_data])
 
-prediction=model.predict(input_vector)
-if prediction[0] == 0:
-    print("🟢 Real News")
-else:
-    print("🔴 Fake News")
+
+
+
+
 
 
 
