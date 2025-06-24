e8f3dccfce0923453e15e609c693e4e639bd6ebf
jessykiruba29 <jessykiruba2006@gmail.com>
2025-06-24 15:02:12 +0530

    model trained

--- train_model.py
+++ train_model.py
@@ -7,46 +7,62 @@ from sklearn.metrics import accuracy_score
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer 
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
 import nltk
+
 nltk.download('stopwords')
 
-file_path = "Fake.csv"
-news_list_real=pd.read_csv("./data/True.csv")
-news_list_fake=pd.read_csv("./data/Fake.csv")
+news=pd.read_csv('./data/news_dataset.csv')
+news=news.dropna(subset=["text", "label"])
+news["label"]=news["label"].astype(str).str.lower().map({"real": 0, "fake": 1})
 
-news_list_real["label"]=0
-news_list_fake["label"]=1
-
-news = pd.concat([news_list_real,news_list_fake], ignore_index=True) #concat both into 1 dataframe
- 
-# REAL => 0
-# FAKE => 1
-
-
-#stemming => to remove prefix suffix, and to only return root word
-stem=PorterStemmer()
+port_stem=PorterStemmer()
+stop_words=set(stopwords.words('english'))
 def stemming(content):
-    
-    stemmed=re.sub('[^a-zA-Z]',' ',content)
-    stemmed=stemmed.lower()
-    words=stemmed.split()
-    stemmed_words=[stem.stem(word) for word in words if word not in stopwords.words('english')]
-    return ' '.join(stemmed_words)
+    content=re.sub('[^a-zA-Z]', ' ', content)
+    content=content.lower()
+    words=content.split()
+    stemmed=[port_stem.stem(word) for word in words if word not in stop_words]
+    return ' '.join(stemmed)
 
-print("Original:", news["text"].iloc[0][:200])
-print("Processed:", stemming(news["text"].iloc[0])[:200])
+news["text"]=news["text"].apply(stemming)
 
-news["text"] = news["text"].apply(stemming)
+X=news["text"]
+Y=news["label"]
 
-X = news["text"]
-Y = news["label"]
+vectorizer=TfidfVectorizer()
+vectorizer.fit(X)
+X=vectorizer.transform(X)
 
-print("✅ X shape:", X.shape)
-print("✅ Y shape:", Y.shape)
-print("✅ First few samples:")
-print(X.head())
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=2)
+
+# model training
+model=LogisticRegression()
+model.fit(X_train,Y_train)
+
+# model evaluation (training)
+X_train_pred=model.predict(X_train)
+training_accuracy=accuracy_score(X_train_pred,Y_train)
+print(training_accuracy)
+
+# model evaluation (testing)
+X_test_pred=model.predict(X_test)
+testing_accuracy=accuracy_score(X_test_pred,Y_test)
+print(testing_accuracy)
+
+#make a predictor system
+input_data="pope passed away"
+input_data=stemming(input_data)
+input_vector=vectorizer.transform([input_data])
+
+prediction=model.predict(input_vector)
+if prediction[0] == 0:
+    print("🟢 Real News")
+else:
+    print("🔴 Fake News")
+
+
+
+
 
 
 
